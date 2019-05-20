98630b1b643b6eb688d9459ceed6ebe4ecb3b838
Feng <bafelem@gmail.com>
2019-05-20 11:52:48 +0800

    memo, recursive

---
+++ fibonacci_memoization.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+const LIM = 41
+
+var fibs [LIM]uint64
+
+func main() {
+	var result uint64 = 0
+	start := time.Now()
+	result = fibonacci(40)
+	fmt.Println(result)
+	end := time.Now()
+	delta := end.Sub(start)
+	fmt.Printf("took this amount of tiem: %s\n", delta)
+}
+
+func fibonacci(n int) (res uint64) {
+	if fibs[n] != 0 {
+		res = fibs[n]
+		return
+	}
+	if n <= 1 {
+		res = 1
+	} else {
+		res = fibonacci(n-1) + fibonacci(n-2)
+	}
+	fibs[n] = res
+	return
+}
